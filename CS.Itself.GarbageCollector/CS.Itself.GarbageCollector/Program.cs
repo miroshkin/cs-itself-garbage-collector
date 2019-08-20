08c0257207a679285832666c79e6a232e09ff8ee
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-08-20 09:43:49 +0300

    Added project

---
+++ CS.Itself.GarbageCollector/CS.Itself.GarbageCollector/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CS.Itself.GarbageCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            A aa = new A();
+            aa = null;
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            B b = new B(new A());
+            b = null;
+        }
+    }
+
+    class A
+    {
+        ~A()
+        {
+            Console.WriteLine("Destructing A class instance");
+        }
+    }
+
+    class B
+    {
+        private object Ref;
+
+        public B(object o)
+        {
+            Ref = o;
+        }
+
+        ~B()
+        {
+            Console.WriteLine("Destucting B class instance");
+        }
+    }
+}
